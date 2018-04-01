21d3d4b9a642ae999946e10f41ae4ca838e0ef7e
Matt Turner <matturner@gmail.com>
2018-04-01 23:14:50 +0100

    DNS fixes

--- terraform/deployments/frogstar-b/deploy/cluster.tf
+++ terraform/deployments/frogstar-b/deploy/cluster.tf
@@ -4,11 +4,20 @@ module "bootstrap" {
   deployment_name = "${var.deployment_name}"
 }
 
+resource "cloudflare_record" "mt165_co_uk_root" {
+  domain  = "mt165.co.uk"
+  name    = "@"
+  type    = "A"
+  value   = "${data.kubernetes_service.nginx_ingress.load_balancer_ingress.0.ip}"
+  ttl     = 120                                                                   # seems to be the shortest CF allows before overriding you to "default"
+  proxied = false
+}
+
 resource "cloudflare_record" "mt165_co_uk_star" {
   domain  = "mt165.co.uk"
   name    = "*"
   type    = "A"
   value   = "${data.kubernetes_service.nginx_ingress.load_balancer_ingress.0.ip}"
-  ttl     = 3600
+  ttl     = 120                                                                   # seems to be the shortest CF allows before overriding you to "default"
   proxied = false
 }
